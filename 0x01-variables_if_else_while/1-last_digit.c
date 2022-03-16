9d1caff766fb7465fa34d9d5cfc951d820769f8d
leantan-get <leantanget2@gmail.com>
2022-03-16 10:58:42 +0000

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - Entry point
  *
  * Return: Always 0 (success/correct)
- *
+
  */
 int main(void)
 
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 
 if(n % 10 > 5)
 {
-printf("Last digit of %i and is greater than 5\n", n , n%10);
+printf("Last digit of %i is %i and is greater than 5\n", n , n%10);
 }
 
 else if (n % 10 == 0)
