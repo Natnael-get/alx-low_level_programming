b5f042c707de6df5e288d25fda2fb4ed0c062007
leantan-get <leantanget2@gmail.com>
2022-03-17 00:38:13 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _is lower - short description, single line
+ * @c: contanins value to be compared
+ * Return: Always 0.
+ */
+int -islower(int c)
+
+{
+
+if (c > 'a' &&  c < 'z')
+{
+return (1);
+}
+else
+{
+return(0);
+}
+
+}
