ae7ae94b2d44e78c9c7a5c0a3b5dc0265b4c8211
leantan-get <leantanget2@gmail.com>
2022-03-18 01:18:31 +0000

    task5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ *return: always 0(success/correct)
+ */
+int main(void)
+{
+
+int i;
+for (i = 40; i <= 57; i++)
+{
+putchar(i);
+}
+putchar("\n");
+
+return (0);
+}
