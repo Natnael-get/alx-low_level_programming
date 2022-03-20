b1c5e9b7cd59d83503963b9fb9ee4923db400814
leantan-get <leantanget2@gmail.com>
2022-03-20 00:48:14 +0000

    taskss

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *Return: Always 0 (success/crrect)
+ */
+int main(void)
+{
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base 16++)
+{
+putchar(base_16);
+}
+putchar ('\n');
+return (0);
+}
