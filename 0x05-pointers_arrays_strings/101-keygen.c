ec256cf3f0cadac2c15a6953ba805b67b7e0360b
leantan-get <leantanget2@gmail.com>
2022-03-22 22:44:59 +0000

    project

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
+strand(time(0));
+num = rand();
+printf("%\n", num);
+return (0);
+}
+
